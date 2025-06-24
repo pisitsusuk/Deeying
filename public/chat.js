04bb9f4b2057e3299e77bc8299353d4c64c4fc8e
pisitsusuk <pisitsusuk@gmail.com>
2025-06-24 23:15:30 +0700

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -38,7 +38,7 @@ async function startChat(event) {
   chatHistory.appendChild(userChat);
 
   try {
- const response = await fetch("https://deeying-1.onrender.com/public/chat", {
+ const response = await fetch("https://deeying-1.onrender.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: userMessage }),
